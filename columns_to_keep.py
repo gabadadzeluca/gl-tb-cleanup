47a9ad55dea1be9276c33a17a9c78e7c19db1871
Luca Gabadadze <gabadadzeluca@gmail.com>
2025-11-19 20:09:34 +0400

    reverse the map; English keys and georgian values for easier access

---
+++ columns_to_keep.py
@@ -0,0 +1,10 @@
+COLUMNS_TO_KEEP = {
+    "date": "თარიღი",
+    "acc_debit": "სადებ.ანგარიში",
+    "name_debit": "დასახელება (დებ.)",
+    "acc_credit": "საკრედ.ანგარიში",
+    "description": "დანიშნულება",
+    "name_credit": "დასახელება (კრედ.)",
+    "amount": "თანხა",
+    "basis": "საფუძველი",
+}
