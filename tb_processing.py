d47aa0df4d26e96650bd09aed07b3c971cc890e2
Luca Gabadadze <gabadadzeluca@gmail.com>
2025-11-22 11:27:32 +0400

    update tb processing file; remove comments

--- tb_processing.py
+++ tb_processing.py
@@ -1,8 +1,8 @@
 import pandas as pd
+from columns_to_keep import COLUMNS_TB
 from common.cleanup import clean_df
 
 def process_tb(df: pd.DataFrame) -> pd.DataFrame:
-   #TODO process TB data
-  df = clean_df(df)
-  ...
+  df = clean_df(df, col_map=COLUMNS_TB)
+
   return df
