65d342de47a3168a8c48823f2c74505e4687e032
Luca Gabadadze <gabadadzeluca@gmail.com>
2025-12-19 20:57:35 +0400

    restrucure main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
-from gl_processing import load_excel, process_gl
+from gl_processing import process_gl
 from tb_processing import process_tb
+from common.cleanup import load_excel
 from add_groupings import add_groupings
 
 RAW_GL_FILENAME = "GL.xlsx"
@@ -18,16 +19,14 @@ def main():
         gl_df = process_gl(gl_df)
         gl_df = add_groupings(gl_df)
         
-        
         # #-----TB processing
-        # tb_df = load_excel(RAW_TB_FILENAME)
-        # tb_df = process_tb(tb_df)
+        tb_df = load_excel(RAW_TB_FILENAME)
+        tb_df = process_tb(tb_df)
 
-        print(gl_df.head())
-         # ---- SAVE BOTH TO SAME WORKBOOK ----
-        with pd.ExcelWriter(OUTPUT_FILENAME, engine="openpyxl") as writer:
+        # ---- SAVE BOTH TO SAME WORKBOOK ----
+        with pd.ExcelWriter(OUTPUT_FILENAME, engine="openpyxl", mode="w") as writer:
             gl_df.to_excel(writer, sheet_name="GL_cleaned", index=False)
-            # tb_df.to_excel(writer, sheet_name="TB_cleaned", index=False)
+            tb_df.to_excel(writer, sheet_name="TB_cleaned", index=False)
 
 
     except FileNotFoundError as e:
