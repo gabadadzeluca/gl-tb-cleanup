c02efe9773b1f7fd74aace836ae45926bf218467
Luca Gabadadze <gabadadzeluca@gmail.com>
2025-12-19 21:06:50 +0400

    rename ppe sheet to ppe_processing

--- ppe.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def add_ppe_sheet(df: pd.DataFrame) -> pd.DataFrame:
-   #TODO add ppe from TB
-
-  return df
---
+++ ppe_processing.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def add_ppe_sheet(df: pd.DataFrame, filename) -> pd.DataFrame:
+   #TODO add ppe from TB
+
+  return df
