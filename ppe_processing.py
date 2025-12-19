cb7caf798fef705b2c64b7c7c0ac943eb8384136
Luca Gabadadze <gabadadzeluca@gmail.com>
2025-12-19 21:13:22 +0400

    add first version of PPE Working sheet

--- ppe_processing.py
+++ ppe_processing.py
@@ -1,6 +1,13 @@
 import pandas as pd
+from columns_to_keep import COLUMNS_TB
 
-def add_ppe_sheet(df: pd.DataFrame, filename) -> pd.DataFrame:
-   #TODO add ppe from TB
+# Test for now
+PPE_PREFIXES = ['16','21']
 
-  return df
+def process_ppe(df: pd.DataFrame) -> pd.DataFrame:
+   acc  = COLUMNS_TB["acc"]
+
+   mask_ppe = df[acc].astype(str).str.startswith(tuple(PPE_PREFIXES))
+   df = df[mask_ppe]
+
+   return df
