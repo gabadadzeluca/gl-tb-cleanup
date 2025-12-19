be14768cc6f7bee7e9719608b621e53686385da1
Luca Gabadadze <gabadadzeluca@gmail.com>
2025-12-19 22:39:31 +0400

    add new rules for medium accounts (shualeduri angarishi) and exclude main accs 000' & 00's

--- ppe_processing.py
+++ ppe_processing.py
@@ -1,13 +1,24 @@
 import pandas as pd
 from columns_to_keep import COLUMNS_TB
 
+# For accessing the left(4) account code
+from tb_processing import ACC_LEFT
+
 # Test for now
-PPE_PREFIXES = ['16','21']
+PPE_PREFIXES = ['163','21']
 
 def process_ppe(df: pd.DataFrame) -> pd.DataFrame:
    acc  = COLUMNS_TB["acc"]
 
-   mask_ppe = df[acc].astype(str).str.startswith(tuple(PPE_PREFIXES))
+   acc_str = df[ACC_LEFT].astype(str)
+   acc_num = pd.to_numeric(acc_str, errors="coerce")
+
+   mask_ppe = (
+      acc_str.str.startswith(tuple(PPE_PREFIXES)) &
+      # Exclude accounts ending with 000, 00
+      (acc_num % 1000 != 0) &
+      (acc_num % 100 != 0)
+   )
    df = df[mask_ppe]
 
    return df
