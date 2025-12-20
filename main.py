ef99eb04b294499382f546bd9c6fded033e087f7
Luca Gabadadze <gabadadzeluca@gmail.com>
2025-12-20 13:49:54 +0400

    update main; move to uploading single file only logic

--- main.py
+++ main.py
@@ -3,28 +3,32 @@ from gl_processing import process_gl
 from tb_processing import process_tb
 from common.cleanup import load_excel
 
-OUTPUT_FILENAME = "Processed_output.xlsx"
 
-
-def main(file_paths):
+def main(file_path, isGL, filename=""):   
+    #Optional file (comapny name)     
+    gl_file, tb_file = None, None
+    OUTPUT_FILENAME = f"TB-{filename}.xlsx"
 
     try:
+        if(isGL):
+            """GL processing"""
+            gl_file = file_path
+            gl_df = load_excel(gl_file)
+            gl_df = process_gl(gl_df)
+            OUTPUT_FILENAME = f"GL-{filename}.xlsx"
+        else:
+            """TB processing"""
+            tb_file = file_path
+            tb_df = load_excel(tb_file)
+            tb_df = process_tb(tb_df)
 
-        gl_file, tb_file = file_paths
-        """GL processing"""
-        gl_df = load_excel(gl_file)
-        print("Excel loaded successfully!")
-
-        gl_df = process_gl(gl_df)
-        
-        """TB processing"""
-        tb_df = load_excel(tb_file)
-        tb_df = process_tb(tb_df)
-
-        # ---- SAVE BOTH TO SAME WORKBOOK ----
+        # ---- SAVE TO WORKBOOK ----
         with pd.ExcelWriter(OUTPUT_FILENAME, engine="openpyxl", mode="w") as writer:
-            gl_df.to_excel(writer, sheet_name="GL_cleaned", index=False)
-            tb_df.to_excel(writer, sheet_name="TB_cleaned", index=False)
+            if isGL:
+                gl_df.to_excel(writer, sheet_name="GL_cleaned", index=False)
+            else:
+                tb_df.to_excel(writer, sheet_name="TB_cleaned", index=False)
+
         return OUTPUT_FILENAME
 
     except FileNotFoundError as e:
