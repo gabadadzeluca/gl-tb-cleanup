6e1bd32d045db89c3af88a0e6d8b85273b5cf216
Luca Gabadadze <gabadadzeluca@gmail.com>
2026-01-01 19:57:55 +0400

    get a class for gui button even keys

--- gui_keys.py
+++ gui_keys.py
@@ -1,1 +1,9 @@
 # a file for keys for gui events
+
+class GUIKeys:
+    PROCESS_TB = "process_tb"
+    PROCESS_GL = "process_gl"
+    PROCESS_RECON = "process_recon"
+    EXIT = "exit_btn"
+    OUTPUT_NAME = "output_name"
+    FILE_PATH = "file_path"
