8a45e0b6f0e0bee6c9351b3248f24d0786cdf8e1
Luca Gabadadze <gabadadzeluca@gmail.com>
2026-01-02 02:39:25 +0400

    add mask for account extraction and change the name of that function;

--- reconciliation.py
+++ reconciliation.py
@@ -4,11 +4,12 @@ from columns_to_keep import COLUMNS_GL
 from columns_to_keep import COLUMNS_TB
 
 
-TB_MASK = [COLUMNS_TB["acc"], COLUMNS_TB["name"], COLUMNS_TB["movement_dr"], COLUMNS_TB["movement_cr"]]
+START_ROW = 4  # leaves rows 1–3 free for headers
+SHEET_NAME = "TB&GL Reconciliation"
 
 def build_recon_skeleton(tb_df):
     return pd.DataFrame({
-        "Account": extract_main_accounts(tb_df),
+        "Account": extract_needed_accounts(tb_df),
         "Description": None,
         "Movement DR (TB)": None,
         "Movement CR (TB)": None,
@@ -18,25 +19,37 @@ def build_recon_skeleton(tb_df):
         "Check CR": None,
     })
 
-def extract_main_accounts(tb_df):
+def extract_needed_accounts(tb_df):
   acc_str = tb_df[COLUMNS_TB["acc"]].str.strip()
-  mask = (
-    acc_str.str.len() == 4
-  ) & (
-    acc_str.astype(int) % 100 == 0
-  )
+
+
+  numeric_acc = pd.to_numeric(acc_str, errors='coerce')
+
+  # accounts that are exactly 4 digits and not ending with 00
+  mask1 = (acc_str.str.len() == 4) & \
+          (acc_str.str.isdigit()) & \
+          (numeric_acc % 100 != 0)
+
+  # accounts that start with letters: eg: B600
+  mask2 = (acc_str.str.len() == 4) & (acc_str.str[:1].str.isalpha())
+  mask = mask1 | mask2
 
   return acc_str[mask]
 
 def reconcile_data(tb_df: pd.DataFrame, gl_df: pd.DataFrame, writer: pd.ExcelWriter) -> None:
-    sheet_name = "TB&GL Reconciliation"
 
     # 1. Build the reconciliation base table (structure only)
+    recon_df = build_recon_skeleton(tb_df)
 
     # 2. Write raw data
-    # recon_df.to_excel(writer, sheet_name=sheet_name, index=False)
+    recon_df.to_excel(
+      writer,
+      sheet_name=SHEET_NAME,
+      index=False,
+      startrow=START_ROW - 1  # pandas is 0-based
+    )
 
-    ws = writer.sheets[sheet_name]
-
+    ws = writer.sheets[SHEET_NAME]
     # 3. Inject Excel formulas
     # add_reconciliation_formulas(ws, recon_df)
+    return recon_df # test
