37ab60781261dc584a30733661640d91eee253d7
Luca Gabadadze <gabadadzeluca@gmail.com>
2026-01-02 00:18:53 +0400

    add a new key

--- models/gui_keys.py
+++ models/gui_keys.py
@@ -3,8 +3,8 @@
 class GUI_KEYS:
     PROCESS_TB = "process_tb"
     PROCESS_GL = "process_gl"
-    PROCESS_RECON = "process_recon"
     EXIT = "exit_btn"
     OUTPUT_NAME = "output_name"
     FILE_PATH = "file_path"
     PROCESS_BOTH = "process_both"
+    UPLOADED_FILES = "uploaded_files"
