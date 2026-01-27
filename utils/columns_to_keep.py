a41e8bd2ed956c51774560562489e9c6acfd13bd
Luca Gabadadze <gabadadzeluca@gmail.com>
2026-01-27 23:09:11 +0400

    add new headings for 1C base worksheets; test only

--- utils/columns_to_keep.py
+++ utils/columns_to_keep.py
@@ -1,13 +1,31 @@
-COLUMNS_GL = {
+COLUMNS_GL_2C = {
     "date": "თარიღი",
     "acc_debit": "სადებ.ანგარიში",
     "name_debit": "დასახელება (დებ.)",
     "acc_credit": "საკრედ.ანგარიში",
     "description": "დანიშნულება",
-    "name_credit": "დასახელება (კრედ.)",
-    "amount": "თანხა",
-    "basis": "საფუძველი",
 }
+
+COLUMNS_GL_1C = {
+    "date": "პერიოდი",
+    "registrator": "რეგისტრატორი",
+    
+    "acc_debit": "ანგარიში დტ",
+    "name_debit": "სუბკონტო 1 დტ",
+    "subconto_dr_2":"სუბკონტო 2 დტ",
+    "subconto_dr_3":"სუბკონტო 3 დტ",
+
+    "acc_credit": "ანგარიში კტ",
+    "name_credit": "სუბკონტო 1 კტ",
+    "subconto_cr_2":"სუბკონტო 2 კტ",
+    "subconto_cr_3":"სუბკონტო 3 კტ",
+
+    "amount": "თანხა",
+    "basis": "შინაარსი",
+    "comment": "კომენტარი",
+    "description": "დანიშნულება",
+}
+
 
 # Needs to be updated to a RAW TB TODO
 # Version 1
